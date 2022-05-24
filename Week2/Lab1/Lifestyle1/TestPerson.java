ba6612a7725e8d0949f420ec02a0d18c771e3bc4
Bruce Bigirwenkya <brucebigiwenkya@gmail.com>
2022-05-24 11:47:12 +0300

    W2L2E2

---
+++ Week2/Lab1/Lifestyle1/TestPerson.java
@@ -0,0 +1,13 @@
+
+public class TestPerson {
+
+	public static void main(String[] args) {
+		// Create Person object and test the methods
+		Person p = new Person();
+		p.setHeight(180);
+		p.setWeight(72);
+		p.setPulse(120);
+		p.displayProfile();
+	}
+
+}
